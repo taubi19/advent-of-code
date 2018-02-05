5dcefc98cf3e12b8d6847b632aee74fda89b52b8
golp <primoz.golob@timetac.com>
2018-02-05 22:31:05 +0100

    - part 2 of 2 - readme

--- 2/README.md
+++ 2/README.md
@@ -2,4 +2,6 @@ Assignment text
 =======================
 The spreadsheet consists of rows of apparently-random numbers. To make sure the recovery process is on the right track, they need you to calculate the spreadsheet's checksum. For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences.
 
-
+Second part
+===============
+It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
--- 2/main.go
+++ 2/main.go
@@ -21,24 +21,65 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	sum := 0
-
+	sum2 := 0
 	for scanner.Scan() {
 		words := strings.Fields(scanner.Text())
 		// find the difference between the smallest and biggest number in words
-		firstEl, err := strconv.Atoi(words[0])
+		sum += diffMaxMin(words)
+		sum2 += divisibleValRes(words)
+	}
+	fmt.Printf("Sum1 = %v\n", sum)
+	fmt.Printf("Sum2 = %v\n", sum2)
+
+}
+
+// Accepts an array of strings that are supposed to be ints. Each element is then checked if it has a even divisor
+// in the array. When such a divisor is found the division result is added to linesum. When all elements have been
+// checked the sum is returned.
+func divisibleValRes(words []string) int {
+	var linesum = 0
+	for i := 0; i < len(words); i++ {
+		eli, err := strconv.Atoi(words[i])
 		check(err)
-		var min, max int = firstEl, firstEl
-		for i := 0; i < len(words); i++ {
-			num, err := strconv.Atoi(words[i])
-			check(err)
-			if min > num {
-				min = num
+		for j := 0; j < len(words); j++ {
+			if i == j {
+				continue
 			}
-			if max < num {
-				max = num
+			elj, err := strconv.Atoi(words[j])
+			check(err)
+			// if el is equally divided by words[i]
+			if eli%elj == 0 {
+				fmt.Printf("eli = %v, elj = %v, eli/elj= %v, eli mod elj= %v \n", eli, elj, eli/elj, eli%elj)
+				linesum += eli / elj
+				continue
+			}
+			// the other way around, words[i] is equally divided by el
+			if elj%eli == 0 {
+				fmt.Printf("other way: eli = %v, elj = %v, eli/elj= %v, eli mod elj= %v \n", eli, elj, elj/eli, elj%eli)
+				linesum += eli / elj
+				continue
 			}
 		}
-		sum += (max - min)
 	}
-	fmt.Println(sum)
+	return linesum
+}
+
+// Accepts an array of strings that are expected to be ints. We search the array for the minimum and maximum, then
+// the difference between those two is calculated and returned.
+func diffMaxMin(words []string) int {
+
+	firstEl, err := strconv.Atoi(words[0])
+	check(err)
+	var min, max int = firstEl, firstEl
+	for i := 0; i < len(words); i++ {
+		num, err := strconv.Atoi(words[i])
+		check(err)
+		if min > num {
+			min = num
+		}
+		if max < num {
+			max = num
+		}
+	}
+	return (max - min)
 }
